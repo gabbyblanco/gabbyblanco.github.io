af18eaa49b98aecabc6b186b51bed33650b86c5d
gabbyblanco <blanco379@stcharles.k12.la.us>
2022-11-10 13:56:49 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -28,18 +28,20 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
             view.addChild(circle);
             circles.push(circle);
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-
+        // drawCircle();
+        // drawCircle();
+        // drawCircle();
+        // drawCircle();
+        // drawCircle();
+for (var loopsCompleted = 0; loopsCompleted , 10; loopsCompleted++){
+    drawCircle();
+}
       
 
         ////////////////////////////////////////////////////////////
@@ -54,7 +56,7 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
-            physikz.updatePosition(circles[0]);
+    physikz.updatePosition(circles[0]);
  	physikz.updatePosition(circles[1]);
  	physikz.updatePosition(circles[2]);
  	physikz.updatePosition(circles[3]);
@@ -93,16 +95,16 @@ var init = function (window) {
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
             if (rightEdge < 0){
-                circle.x = +circle.radius;
+                circle.x = canvas. width +circle.radius;
             }
             if (leftEdge > canvas.width){
-                circle.x = - circle.radius;
+                circle.x = -circle.radius;
             }
             if (topEdge > canvas.height){
                 circle.y = -circle.radius;
             }
             if (bottomEdge < 0 ){
-                circle.y = +circle.radius;
+                circle.y = canvas.width +circle.radius;
             }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
